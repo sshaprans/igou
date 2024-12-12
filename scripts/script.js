cca322a8d469114f68e09ebd21c431553d517614
Srhii Shapran <sshaprans@gmail.com>
2024-12-12 21:41:47 +0200

    added new page

--- scripts/script.js
+++ scripts/script.js
@@ -15,4 +15,30 @@ btnFixed.addEventListener('click', () => {
 
 // edition
 
+// Знаходимо всі кнопки з класом .open-modal
+const openModalButtons = document.querySelectorAll('.open-modal');
+
+// Знаходимо модальне вікно і кнопку для закриття
+const modal = document.getElementById('modal');
+const closeModalButton = document.querySelector('.close');
+
+// Додаємо обробник подій для кожної кнопки
+openModalButtons.forEach(button => {
+    button.addEventListener('click', function (event) {
+        event.preventDefault(); // Запобігаємо переходу за посиланням
+        modal.style.display = 'block'; // Показуємо модальне вікно
+    });
+});
+
+// Закриваємо модальне вікно при натисканні на "Х"
+closeModalButton.addEventListener('click', function () {
+    modal.style.display = 'none';
+});
+
+// Закриваємо модальне вікно при натисканні за межами вікна
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
 
